d329e6c4616c7962be157e60e722d7c4205ace88
reubenleyland <112395437+reubenleyland@users.noreply.github.com>
2023-05-22 16:52:05 +0100

    Ex2
    Ex2

--- assignment2/plot.py
+++ assignment2/plot.py
@@ -3,15 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # parameters to modify
-filename="test.data"
-label='label'
+filename="pingdataform.txt"
+label='pingtest'
 xlabel = 'xlabel'
 ylabel = 'ylabel'
-title='Simple plot'
-fig_name='test.png'
+title='Ping:i0.01'
+fig_name='test1.png'
 
 
 t = np.loadtxt(filename, delimiter=" ", dtype="float")
+
+
 
 plt.plot(t[:,0], t[:,1], label=label)  # Plot some data on the (implicit) axes.
 plt.xlabel(xlabel)
