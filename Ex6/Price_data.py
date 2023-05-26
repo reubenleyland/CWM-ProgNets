e19d5a19b74eb0e25a641db0088c4d4ec1beb926
reubenleyland <112395437+reubenleyland@users.noreply.github.com>
2023-05-26 12:08:57 +0100

    Ex6

--- Ex6/Price_data.py
+++ Ex6/Price_data.py
@@ -5,13 +5,13 @@ import random
 import time
 from scapy.all import *
 
-src_ip= "192.168.10.1"
-dst_ip= "192.168.10.2"
+#src_ip= "192.168.10.1"
+#dst_ip= "192.168.10.2"
 src_MAC="0c:37:96:5f:89:d4"
 dst_MAC= "e4:5f:01:84:8c:86"
-ip_layer = IP()
-ip_layer.src =src_ip
-ip_layer.dst = dst_ip
+#ip_layer = IP()
+#ip_layer.src =src_ip
+#ip_layer.dst = dst_ip
 
 
 class price_data(Packet):
@@ -26,10 +26,10 @@ eth_layer.dst = dst_MAC
 eth_layer.type= 0x1234
 i=0
 for i in range(0,10):
-	packet = eth_layer/ip_layer/price_data(price=random.randrange(0,100),time=i)
+	packet = eth_layer/price_data(price=random.randrange(0,100),time=i)
 	i+=1
 	print(packet.summary)
 	sendp(packet,iface="enx0c37965f89d4")
-	time.sleep(1)
+	time.sleep(3)
 
 
