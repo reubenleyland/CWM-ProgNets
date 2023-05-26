304553e77e03980f1d1ac242714e2f4a4fb2f8fb
reubenleyland <112395437+reubenleyland@users.noreply.github.com>
2023-05-26 11:32:05 +0100

    Ex6

--- Ex6/Price_data.py
+++ Ex6/Price_data.py
@@ -2,7 +2,15 @@
 
 import re
 import random 
+import time
 from scapy.all import *
+
+src_ip= "192.168.10.1"
+dst_ip= "192.168.10.2"
+ip_layer = IP()
+ip_layer.src =src_ip
+ip_layer.dst = dst_ip
+
 
 class price_data(Packet):
 	name = "price_data"
@@ -10,11 +18,13 @@ class price_data(Packet):
                         IntField("time", 0),
                         IntField("signal", 0),]
 
-eth_pkt =Ether(dst='00:04:00:00:00:00', type=0x1234)
+eth_pkt =Ether(dst='e4:5f:01:84:8c:86', type=0x1234)
 i=0
 for i in range(0,10):
-	packet = eth_pkt/price_data(price=random.randrange(0,100),time=i)
+	packet = eth_pkt/ip_layer/price_data(price=random.randrange(0,100),time=i)
 	i+=1
 	print(packet.summary)
-	
+	#sendp(packet,iface="eth0")
+	time.sleep(1)
+
 
